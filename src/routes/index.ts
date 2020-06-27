9519311e6e1f6f0eae1488d15be771b061cede11
Yago Cunha <yago.cunha123@gmail.com>
2020-06-27 14:41:53 -0300

    Criaçao de usuario com criptografia de senha

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,11 @@
 // src/routes/index.ts
 import { Router } from 'express';
 import appointments from './appointments.routes';
+import usersRouter from './user.routes';
 
 const routes = Router();
 
 routes.use('/appointments', appointments);
+routes.use('/users', usersRouter);
 
 export default routes;

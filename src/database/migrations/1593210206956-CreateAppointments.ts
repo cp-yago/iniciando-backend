35d8f67813eb5874daf6fdf93b3d1bb097e9093a
Yago Cunha <yago.cunha123@gmail.com>
2020-06-26 21:32:19 -0300

    Repositorio do typeorm, inserção no banco

--- src/database/migrations/1593210206956-CreateAppointments.ts
+++ src/database/migrations/1593210206956-CreateAppointments.ts
@@ -12,6 +12,7 @@ export default class CreateAppointments1593210206956
             type: 'varchar',
             isPrimary: true,
             generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
           },
           {
             name: 'provider',

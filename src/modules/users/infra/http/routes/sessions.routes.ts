d1201a08ee7be0fda522522a4458b5eb5942ea59
Yago Cunha <yago.cunha123@gmail.com>
2020-08-12 21:13:38 -0300

    Utilizando class transformer

--- src/modules/users/infra/http/routes/sessions.routes.ts
+++ src/modules/users/infra/http/routes/sessions.routes.ts
@@ -12,7 +12,7 @@ sessionsRouter.post(
     [Segments.BODY]: {
       email: Joi.string().email().required(),
       password: Joi.string().required()
-    }
+    },
   }),
   sessionsController.create
 );
